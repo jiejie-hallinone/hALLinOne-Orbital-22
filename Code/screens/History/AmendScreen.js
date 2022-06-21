e98165214ae72cd85928d6ef31327533d9490d36
Rui Jie <koh.ruijie123@gmail.com>
2022-06-21 22:47:47 +0800

    bookings can be amended

--- Code/screens/History/AmendScreen.js
+++ Code/screens/History/AmendScreen.js
@@ -1,15 +1,198 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+import { amend } from './HistoryScreen'
+import { auth, db } from '../../Firebase/Firebase';
+import { doc, updateDoc, collection } from "firebase/firestore";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useNavigation } from '@react-navigation/native'
 
 // users will be brought here to ammend their selected exising booking from the previous page
 const AmendScreen = () => {
-  return (
-    <View>
-      <Text>AmendScreen</Text>
-    </View>
-  )
+// obtains user info used to make booking
+const currentUser = auth.currentUser;
+const uid = currentUser.uid;
+const name = currentUser.displayName;
+
+// to navigate
+const navigation = useNavigation();
+
+// states used to capture information for booking
+// start date and time
+const [date, setDate] = useState(new Date());
+// end date and time
+const [dateEnd, setDateEnd] = useState(new Date());
+// show date or time picker
+const [mode, setMode] = useState('date')
+// toggle visibility of picker for start time and date
+const [showStart, setShowStart] = useState(false)
+// toggle visibility of picker for end time and date
+const [showEnd, setShowEnd] = useState(false)
+// text for checking selected date and time
+const [text, setText] = useState("Start Date:\nStart time:")
+const [textEnd, setTextEnd] = useState("End Date:\nEnd time:")
+
+// updates start date and time when changed
+const onChangeStart = (event, selectedDate) => {
+  const currentDate = selectedDate || date;
+  setDate(currentDate);
+
+  // shows selected start date and time for debugging
+  let tempDate = new Date(currentDate);
+  let fullDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
+  let fullTime = tempDate.getHours() + 'hrs ' + tempDate.getMinutes() + 'min';
+  setText("Start Date: " + fullDate + '\n' + "Start Time: " + fullTime);
 }
 
-export default AmendScreen
+// updates end date and time when changed
+const onChangeEnd = (event, selectedDate) => {
+  const currentDate = selectedDate || date;
+  setDateEnd(currentDate);
 
-const styles = StyleSheet.create({})
+  // shows selected end date and time for debugging
+  let tempDate = new Date(currentDate);
+  let fullDate = tempDate.getDate() + '/' + (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
+  let fullTime = tempDate.getHours() + 'hrs ' + tempDate.getMinutes() + 'min';
+  setTextEnd("End Date: " + fullDate + '\n' + "End Time: " + fullTime);
+}
+
+// toggles mode and visibility for start picker (makes end invisible)
+const showModeStart = (currentMode) => {
+  setShowEnd(false);
+  setShowStart(true);
+  setMode(currentMode);
+}
+
+// toggles mode and visibility for end picker (makes start invisible)
+const showModeEnd = (currentMode) => {
+  setShowStart(false);
+  setShowEnd(true);
+  setMode(currentMode);
+}
+
+return (
+  <View style={styles.container}>
+    <Text>{text}</Text>
+    
+    <Text>{textEnd}</Text>
+
+    <TouchableOpacity
+      // for selecting start date - shows start picker on date mode
+      title='Select Start Date'
+      onPress={() => showModeStart('date')}
+      style={styles.button}
+     >
+       <Text>Select Start Date </Text> 
+     </TouchableOpacity> 
+
+    <TouchableOpacity
+    // for selecting start time - shows start picker on time mode
+      onPress={() => showModeStart('time')}
+      style={styles.button}
+    > 
+      <Text>Select Start Time </Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity
+    // for selecting end date - shows end picker on date mode
+      onPress={() => showModeEnd('date')}
+      style={styles.button}
+    > 
+      <Text>Select End Date </Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity
+      // for selecting end time - shows end picker on time mode
+      onPress={() => showModeEnd('time')}
+      style={styles.button}
+    > 
+      <Text>Select End Time </Text>
+    </TouchableOpacity>
+
+    {showStart && (
+      <DateTimePicker
+      // start picker, only shows when start is supposed to be visible (showStart === true)
+        testID='Start'
+        value={date}
+        mode={mode}
+        is24Hour={false}
+        display='default'
+        onChange={onChangeStart}
+        style={styles.dtpicker}
+      />
+    )}
+
+    {showEnd && (
+      <DateTimePicker
+      // end picker, only shows when end is supposed to be visible (showEnd === true)
+        testID='End'
+        value={dateEnd}
+        mode={mode}
+        is24Hour={false}
+        display='default'
+        onChange={onChangeEnd}
+        style={styles.dtpicker}
+        minimumDate={date}
+      />
+    )}
+
+    <TouchableOpacity
+    // makes booking by saving into firestore bookings collection
+      onPress={() => {
+          const docRef = updateDoc(doc(db, "bookings", amend), {
+            startDateTime: date,
+            endDateTime: dateEnd,
+          })
+          .catch(err => {
+            alert("Booking does not exist / Unable to update")
+          })
+          .then(() => {
+            console.log("Booking updated");
+            // user notified of successful booking and brought back to main page
+            alert("Booking successfully updated")
+            navigation.navigate("Bookings")
+          })
+         
+          
+      }}
+        
+      style={styles.confirm}
+     >
+       <Text style={styles.text}>Amend Booking</Text>
+    </TouchableOpacity> 
+  </View>
+)
+}
+
+export default AmendScreen;
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  alignItems: 'center',
+},
+button: {
+  backgroundColor: 'white',
+  width: '100%',
+  padding: 15,
+  alignItems: 'center',
+  marginTop: 10,
+},
+dtpicker: {
+  width: '80%',
+  backgroundColor: 'white',
+  marginTop: 20,
+},
+confirm: {
+  width: '50%',
+  backgroundColor:'#0782F9',
+  alignItems: 'center',
+  padding: 15,
+  marginTop: 20,
+  borderRadius: 10,
+},
+text: {
+  color: 'white',
+  fontWeight: '700',
+  fontSize: 16
+}
+})
--- Code/screens/History/HistoryScreen.js
+++ Code/screens/History/HistoryScreen.js
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity } from 'react-native'
 import React, {useEffect, useState} from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { auth, db } from '../../Firebase/Firebase';
 import { doc, getDocs, collection, query, where } from "firebase/firestore";
 
+// amend to be set to id of chosen booking to amend, and exported for amend screen, currently set to a fixed doc
+var amend = "MAS63OBynxCpskjhOVHn";
 
 // here the existing bookings made by the user will be shown
 const HistoryScreen = () => {
   const currentUser = auth.currentUser;
   const uid = currentUser.uid;
+
+  const navigation = useNavigation();
   
   // gets the bookings from firestore made by current user, with end date and time after the current date and time
   const getBookings = async () => {
@@ -44,11 +48,23 @@ const HistoryScreen = () => {
         )}
         keyExtractor={item => item.id}
       />
+
+      <TouchableOpacity
+      // button to amend booking
+          onPress={() => {
+            navigation.navigate("Amend")
+          }}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Amend</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default HistoryScreen
+
+export {amend};
 
 const styles = StyleSheet.create({
   container: {
@@ -67,5 +83,22 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 16,
     fontWeight: 700,
-  }
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  },
 })
