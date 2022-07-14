f68bd393c07be8d8f08fbaa79ea430b2abd0ab31
Rui Jie <koh.ruijie123@gmail.com>
2022-07-15 03:49:44 +0800

    can create event on native calendar navigation not working after confirming booking

--- Code/screens/Bookings/BookScreen.js
+++ Code/screens/Bookings/BookScreen.js
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native'
 import React, {useState} from 'react'
 import { auth, db } from '../../Firebase/Firebase';
-import { doc, addDoc, collection } from "firebase/firestore";
+import { doc, addDoc, collection, onSnapshot, updateDoc } from "firebase/firestore";
 import DateTimePicker from '@react-native-community/datetimepicker';
-// import { google } from 'googleapis';
+import * as Calendar from 'expo-calendar';
 
 // this screen is to fill in the details of the booking of the facility selected in previous page - date and time
 const BookScreen = ({route, navigation}) => {
@@ -12,17 +12,126 @@ const BookScreen = ({route, navigation}) => {
   const uid = currentUser.uid;
   const name = currentUser.displayName;
 
-  const [accessToken, setAccessToken] = useState('')
+  const[calendarId, setCalendarId] = useState('');
+  const[BookingId, setBookingId] = useState('');
   const unsub = onSnapshot(doc(db, "users", auth.currentUser.uid), (doc) => {
     const data = doc.data();
-    if (data) {
-      setAccessToken(data.accessToken);
+    if (data && data.calendarId) {
+      setCalendarId(data.calendarId);
     } else {
-      setAccessToken('');
+      setCalendarId('');
     }
   }); 
 
+  async function getDefaultCalendarSource() {
+    const defaultCalendar = await Calendar.getDefaultCalendarAsync();
+    return defaultCalendar.source;
+  }
+  
+  async function createCalendar() {
+    const defaultCalendarSource =
+      Platform.OS === 'ios'
+        ? await getDefaultCalendarSource()
+        : { isLocalAccount: true, name: 'Hall Bookings' };
+    const newCalendarID = await Calendar.createCalendarAsync({
+      title: 'Hall Bookings',
+      color: 'blue',
+      entityType: Calendar.EntityTypes.EVENT,
+      sourceId: defaultCalendarSource.id,
+      source: defaultCalendarSource,
+      name: 'hALLinOne',
+      ownerAccount: 'personal',
+      accessLevel: Calendar.CalendarAccessLevel.OWNER,
+    });
+    // console.log(`${newCalendarID}`);
+    return `${newCalendarID}`;
+  }
+
+  async function createEvent(calendarId) {
+    const newEventID = await Calendar.createEventAsync(calendarId, {
+      title: "Booking",
+      location: paramsToLocation(),
+      startDate: date,
+      endDate: dateEnd,
+      calendarId: calendarId,
+      notes: "added from hALLinOne application"
+    })
+    return `${newEventID}`; 
+  }
+
   const {hall, block, facility} = route.params;
+
+  // to match abbreviation to full hall name (since we stored abbrev to save space in firestore)
+const hallName = hallAbbreviation => {
+  if (hallAbbreviation === "TH") {
+    return "Temasek Hall";
+  }
+  else if (hallAbbreviation === "EH") {
+    return "Eusoff Hall";
+  }
+  else if (hallAbbreviation === "SH") {
+    return "Sheares Hall";
+  }
+  else if (hallAbbreviation === "KR") {
+    return "Kent Ridge Hall";
+  }
+  else if (hallAbbreviation === "LH") {
+    return "Prince George's Park Hall";
+  }
+  else {
+    return "King Edwards VII Hall"
+  }
+}
+
+// to match abbreviation to letter / number for blocks(since we stored only letters to save space in firestore)
+const blockName = letter => {
+  if (letter === "A") {
+    return "A / 1";
+  }
+  else if (letter === "B") {
+    return "B / 2";
+  }
+  else if (letter === "C") {
+    return "C / 3";
+  }
+  else if (letter === "D") {
+    return "D / 4";
+  }
+  else if (letter === 'E') {
+    return "E / 5";
+  } else {
+    return "";
+  }
+}
+
+// to match abbreviation to full facility name (since we stored abbrev to save space in firestore)
+const facName = facAbbreviation => {
+  if (facAbbreviation === 'L') {
+    return "Lounge";
+  }
+  else if (facAbbreviation === 'W') {
+    return "Washing Machine";
+  }
+  else if (facAbbreviation === 'D') {
+    return "Dryer";
+  }
+  else if (facAbbreviation === 'B') {
+    return "Basketball Court";
+  }
+  else if (facAbbreviation === 'S') {
+    return "Squash Court";
+  }
+  else if (facAbbreviation === 'M') {
+    return "Band / Music Room";
+  }
+  else {
+    return "Communal Hall"
+  }
+}
+
+  const paramsToLocation = () => {
+    return hallName(hall) + " " + blockName(block) + " " + facName(facility);
+  }
 
   // states used to capture information for booking
   // start date and time
@@ -146,8 +255,8 @@ const BookScreen = ({route, navigation}) => {
 
       <TouchableOpacity
       // makes booking by saving into firestore bookings collection
-        onPress={() => {
-            const docRef = addDoc(collection(db, "bookings"), {
+        onPress={async () => {
+          const docRef = await addDoc(collection(db, "bookings"), {
             uid: uid,
             name: name,
             hall: hall,
@@ -156,34 +265,56 @@ const BookScreen = ({route, navigation}) => {
             startDateTime: date,
             endDateTime: dateEnd,
           })
+          console.log("ID:" + docRef?.id);
+          setBookingId(docRef?.id);
           console.log("Booking made");
           // user notified of successful booking and brought back to main page
-          Alert.alert('Booking successfully made', 'Add to Google Calendar?', [
-            {text:'Add to Google Calendar', onPress: () => {
-              if (accessToken === '') {
-                alert("No Google Account Linked! Proceed to Settings to link account.")
-              } else {
-                var event = {
-                  'summary': 'Booking',
-                  'location': 'test',
-                  'start': date,
-                  'end': dateEnd
-                }
-                /*
-                const calendar = google.calendar({version: 'v3', auth: accessToken})
-                calendar.events.insert({
-                  auth: accessToken,
-                  calendarId: 'primary',
-                  resource: event,
-                }, (err, event) => {
-                  if (err) {
-                    console.log('Error contacting Calender service: ' + err);
+          Alert.alert('Booking successfully made', 'Add to Calendar?', [
+            {text:'Add to Calendar', onPress: () => {
+              (async () => {
+                const { status } = await Calendar.requestCalendarPermissionsAsync();
+                // permission to access calendar granted
+                if (status === 'granted') {
+                  // yet to create a new calendar for hall bookings
+                    if (calendarId === '') {
+                    // creates a calendar in device native calendar, named Hall Bookings. calender represents ID of calendar
+                    const calendar = await createCalendar();
+                    setCalendarId(calendar)
+                    console.log("calender " + calendar);
+                    const docRef = updateDoc(doc(db, 'users', uid), {
+                      calendarId: calendar
+                    })
+                    .catch(err => {
+                      alert("Unable to update calendar")
+                    })
+                    // create event for booking
+                    const event = await createEvent();
+                    console.log("event created with ID: " + event);
+                    const docRef2 = updateDoc(doc(db, 'bookings', BookingId), {
+                      eventID: event
+                    })
+                    .catch(err => {
+                      alert("Unable to update calendar")
+                    })
+                  // hall bookings calendar created, just create event
                   } else {
-                    console.log('Event created')
+                    const event = await createEvent(calendarId);
+                    console.log("event created with ID: " + event);
+                    const docRef = updateDoc(doc(db, 'bookings', BookingId), {
+                      eventID: event
+                    })
+                    .catch(err => {
+                      alert("Unable to update calendar")
+                    })
                   }
-                })
-                */
-              }
+                }
+                else {
+                  alert("Unable to access calendar")
+                }
+              })();
+              navigation.navigate("History", {
+                amended: true
+              });
             }},
             {text:'No', onPress: () => {
               navigation.navigate("History", {
